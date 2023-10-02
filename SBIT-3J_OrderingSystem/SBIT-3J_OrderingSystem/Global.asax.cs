afc50663714901075ddb5e00109cb2cbbd1dd1e5
Jhonjhorie <74289488+Jhonjhorie@users.noreply.github.com>
2023-10-02 18:27:23 +0800

    Start

---
+++ SBIT-3J_OrderingSystem/SBIT-3J_OrderingSystem/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace SBIT_3J_OrderingSystem
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
