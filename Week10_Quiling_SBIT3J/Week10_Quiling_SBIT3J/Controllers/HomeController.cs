32daed46e650f5ff52f267909d8840809e570fb2
Jhonjhorie <74289488+Jhonjhorie@users.noreply.github.com>
2023-10-23 20:21:46 +0800

    ss

---
+++ Week10_Quiling_SBIT3J/Week10_Quiling_SBIT3J/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Week10_Quiling_SBIT3J.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About";
+
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            ViewBag.Message = "Projects";
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact";
+
+            return View();
+        }
+
+
+
+
+    }
+}
---
+++ Week10_Quiling_SBIT3J/Week10_Quiling_SBIT3J/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Week10_Quiling_SBIT3J.MvcApplication" Language="C#" %>
